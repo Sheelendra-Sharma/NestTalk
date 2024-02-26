325bd7eafbe5d17f5d2e7bac6784515fda60acd6
Sheelendra Sharma <sheelendrasharma15@gmail.com>
2024-02-26 22:17:04 +0530

    Lobby UI Fix

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -34,58 +34,40 @@ const LobbyScreen = () => {
   }, [socket, handleJoinRoom]);
 
   return (
-    // <div >
-    //   <h1 >Lobby</h1>
-    //   <form onSubmit={handleSubmitForm}>
-    //     <label htmlFor="email">Email ID</label>
-    //     <input
-    //       type="email"
-    //       id="email"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //     />
-    //     <br />
-    //     <label htmlFor="room">Room Number</label>
-    //     <input
-    //       type="text"
-    //       id="room"
-    //       value={room}
-    //       onChange={(e) => setRoom(e.target.value)}
-    //     />
-    //     <br />
-    //     <button>Join</button>
-    //   </form>
-    // </div>
-
+    
     <div>
       <form onSubmit={handleSubmitForm}>
-      <div className="bg-blue-700 h-screen min-h-screen flex items-center justify-center">
-      <div className="relative">
-        {/* <h1>Webrtc Video Calling System</h1> */}
-      </div>
-      <div className="bg-white rounded-lg p-4 max-w-lg"> 
-      <div className="flex justify-center font-bold text-2xl">Login</div>
-      <br />
-        <div className="m-4">
-          <label htmlFor="email">Email</label>
-          <input className="border border-gray-500 m-2 rounded-lg bg-gray-200" 
-            type="text" 
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+      <div className="bg-blue-700 h-screen grid">
+        <div className="flex items-center justify-center">
+          <h1 className="text-white text-4xl font-bold">Webrtc Video Calling System</h1>
+        </div>
+        <div className="flex justify-center items-center">
+        <div className="bg-white rounded-lg sm:p-4 max-w-lg inline-block"> 
+        <div className="flex justify-center font-bold text-2xl sm:p-0 pt-4">Login</div>
+        <br />
+          <div className="m-4 flex justify-between items-center">
+            <label htmlFor="email">Email</label>
+            <input className="border border-gray-500 m-2 rounded-lg bg-gray-200" 
+              type="text" 
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
+          </div>
+          <div className="m-4 flex justify-between items-center">
+          <label htmlFor="room">Room No.</label>
+            <input className="border border-gray-500 m-2 rounded-lg bg-gray-200" 
+              type="text" 
+              id="room"
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
             />
+          </div>
         </div>
-        <div className="m-4">
-        <label htmlFor="room">Room No.</label>
-          <input className="border border-gray-500 m-2 rounded-lg bg-gray-200" 
-            type="text" 
-            id="room"
-            value={room}
-            onChange={(e) => setRoom(e.target.value)}
-          />
         </div>
-        <button className="border p-1 border-gray-700 rounded-lg">Join</button>
-      </div>
+        <div className="flex justify-center">
+          <button className="border px-5 py-2.5 h-fit border-gray-700 text-white bg-gradient-to-br from-green-400 to-blue-600 rounded-full font-medium">Join</button>
+        </div>
     </div>
       </form>
     </div>
